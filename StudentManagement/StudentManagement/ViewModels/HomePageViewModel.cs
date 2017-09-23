a77ff4e94b48f921575317b3dd33a96fe6e54656
Ken Huynh <huynhhoatrungkien@gmail.com>
2017-09-24 00:08:09 +0700

    Setup MVVM Prism

---
+++ StudentManagement/StudentManagement/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using StudentManagement.Helpers;
+using StudentManagement.ViewModels.Base;
+
+namespace StudentManagement.ViewModels
+{
+    public class HomePageViewModel : ViewModelBase
+    {
+        #region priavte properties
+
+
+        #endregion
+
+        #region public properties
+        public DelegateCommand OkCommand { get; set; }
+        
+
+        #endregion
+
+
+        public HomePageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            PageTitle = "Home Page";
+            OkCommand = new DelegateCommand(OkExecute);
+        }
+
+        #region Methods
+
+        private void OkExecute()
+        {
+            NavigationService.NavigateAsync(PageManager.HomePage);
+        }
+
+        #endregion
+    }
+}
